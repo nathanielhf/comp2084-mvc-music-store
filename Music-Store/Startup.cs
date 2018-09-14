f2555c6fa21b405ba23626925cac3c0a66ecd6cf
Nathaniel Fisher <nathaniel.fisher@mygeorgian.ca>
2018-09-14 12:31:43 -0400

    first commit

---
+++ Music-Store/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Music_Store.Startup))]
+namespace Music_Store
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
